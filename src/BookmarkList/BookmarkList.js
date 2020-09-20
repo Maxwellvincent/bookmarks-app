e7ba1af75cad7f074fb58ee254dda84356e7d27b
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-09-19 23:52:03 -0400

    updated files

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import PropTypes from 'prop-types';
+import BookmarksContext from '../BookmarksContext';
+import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
+  static proptTypes = {
+    bookmarks: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.oneOfType([
+          PropTypes.number,
+          PropTypes.string,
+        ]).isRequired,
+      })
+    )
+  };
+
   static defaultProps = {
     bookmarks: []
   };
 
+  static contextType = BookmarksContext;
+
   render() {
-    const { bookmarks } = this.props
+    const { bookmarks } = this.context
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
@@ -25,7 +39,5 @@ class BookmarkList extends Component {
     );
   }
 }
-BookmarkList.propTypes = {
-  bookmarks: PropTypes.array
-}
+
 export default BookmarkList;
